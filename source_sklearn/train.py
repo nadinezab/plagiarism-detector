7da21b1e460b7c7ef29ffe23066476c2b781bb25
Nadine Amersi-Belton <nzamersi@gmail.com>
2020-09-14 16:09:39 +0100

    Completed model training and eval

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -8,6 +8,8 @@ from sklearn.externals import joblib
 
 ## TODO: Import any additional libraries you need to define a model
 
+# Import RandomForrestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 # Provided model load function
 def model_fn(model_dir):
@@ -39,6 +41,9 @@ if __name__ == '__main__':
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
     
     ## TODO: Add any additional arguments that you will need to pass into your model
+    # Set number of estimators and max depth hyperparameters
+    parser.add_argument('--n_estimators', type = int, default = 25)
+    parser.add_argument('--max_depth', type = int, default = 10)
     
     # args holds all passed-in arguments
     args = parser.parse_args()
@@ -56,11 +61,13 @@ if __name__ == '__main__':
     
 
     ## TODO: Define a model 
-    model = None
+    model = RandomForestClassifier(n_estimators = args.n_estimators, 
+                                    max_depth = args.max_depth, 
+                                    random_state = 1)
     
     
     ## TODO: Train the model
-    
+    model.fit(train_x, train_y)
     
     
     ## --- End of your code  --- ##
